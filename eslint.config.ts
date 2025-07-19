552ddc3c7d07ca8b5a1e24e79e4242607d12a03a
Cheah Eng Teong <ceteongvanness@hotmail.com>
2025-07-19 10:50:14 +0800

    Create eslint.config.ts

---
+++ eslint.config.ts
@@ -0,0 +1,63 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  env: {
+    node: true,
+    es6: true,
+    jest: true
+  },
+  rules: {
+    // TypeScript specific rules
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    '@typescript-eslint/prefer-const': 'error',
+    '@typescript-eslint/no-unused-expressions': 'error',
+
+    // General rules
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'no-duplicate-imports': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-template': 'error',
+    'object-shorthand': 'error',
+    'arrow-spacing': 'error',
+    'comma-dangle': ['error', 'never'],
+    'quotes': ['error', 'single', { avoidEscape: true }],
+    'semi': ['error', 'always'],
+
+    // Prettier rules
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'none',
+        tabWidth: 2,
+        semi: true,
+        printWidth: 100,
+        endOfLine: 'lf'
+      }
+    ]
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+    'coverage/',
+    '*.js',
+    '!eslint.config.js',
+    '!jest.config.js'
+  ]
+};
